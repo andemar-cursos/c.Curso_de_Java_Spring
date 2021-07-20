5a39f792fd0db32e407af5f101fbe759bd324b53
Anderson <andemartinezdev@gmail.com>
2021-07-20 13:46:10 -0500

    05-05 -> Swagger2
    - Se configuro el swagger para implementar la documentacion de los controllers.

--- 01-Market/src/main/java/com/cursos/andemar/market/web/controller/ProductController.java
+++ 01-Market/src/main/java/com/cursos/andemar/market/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.cursos.andemar.market.web.controller;
 
 import com.cursos.andemar.market.domain.Product;
 import com.cursos.andemar.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +23,21 @@ public class ProductController {
 
 
     @GetMapping("/all")
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Product>> getAll() {
 
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK) ;
     }
 
     @GetMapping("/{productId}")
-    public ResponseEntity<Product> getProduct(@PathVariable("productId") int productId) {
+    @ApiOperation("Search a product with an ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Product not found")
+    })
+    public ResponseEntity<Product> getProduct(    @ApiParam(value = "The id of the product", required = true, example = "7")
+                                                  @PathVariable("productId") int productId) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
