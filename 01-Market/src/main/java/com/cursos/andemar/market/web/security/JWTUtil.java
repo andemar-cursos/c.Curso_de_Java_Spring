8ee112f23d9be46b86ec7d51d4ad465440169ef4
Anderson <andemartinezdev@gmail.com>
2021-07-20 13:59:22 -0500

    06-02 -> JWT
    - Se creo la configuracion del JWT.

---
+++ 01-Market/src/main/java/com/cursos/andemar/market/web/security/JWTUtil.java
@@ -0,0 +1,20 @@
+package com.cursos.andemar.market.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public class JWTUtil {
+
+    private static final String KEY = "4Nd4m4r";
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.ES256, KEY).compact();
+    }
+}
