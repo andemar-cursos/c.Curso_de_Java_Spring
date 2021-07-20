68d02ad6d25d264986d9cdd62913211cd7dfaff1
Anderson <andemartinezdev@gmail.com>
2021-07-20 13:53:23 -0500

    06-01 -> Spring Security
    - Se implemento la seguridad en el WS.

---
+++ 01-Market/src/main/java/com/cursos/andemar/market/web/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.cursos.andemar.market.web.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+}
